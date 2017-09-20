baaf507259d14f6da7dfbd4e763d55835631f18d
Adam Haymond <coastgrd@gmail.com>
2017-09-20 12:43:50 -0600

    updated to latest daniel upstream

--- bash/.bash/aliases/go.aliases.bash
+++ bash/.bash/aliases/go.aliases.bash
@@ -6,3 +6,11 @@ alias gra="go run *.go"
 
 # Run main.go file in the current directory
 alias grm="go run main.go"
+
+function go_build_alpine() {
+  if [ ! -z "$1" ]; then
+    CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-s' -installsuffix cgo -o $1
+  else
+    CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-s' -installsuffix cgo
+  fi
+}
